feeee88fe81d0c81f0b505fb553b17f77aded043
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-03-03 11:56:48 +0200

    Added product in PRODUCTS.js

---
+++ src/PRODUCTS.js
@@ -0,0 +1,59 @@
+import american from "./images/americanCoffee.jpg";
+import brazil from "./images/brazilCoffee.jpg";
+import french from "./images/frenchCoffee.jpg";
+import italian from "./images/italianCoffee.jpg";
+import kenyan from "./images/kenyaCoffee.jpg";
+import morocan from "./images/morocanCoffee.jpg";
+import neworleans from "./images/newOrleansCoffee.jpg";
+import viennese from "./images/vienneseCoffee.jpg";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        name: "American coffee",
+        price: 10,
+        image:  {american},
+    },
+    {
+        id: 2,
+        name: "Brazilian coffee",
+        price: 10,
+        image:  {brazil},
+    },
+    {
+        id: 3,
+        name: "French coffee",
+        price: 10,
+        image:  {french},
+    },
+    {
+        id: 4,
+        name: "Italian coffee",
+        price: 10,
+        image:  {italian},
+    },
+    {
+        id: 5,
+        name: "Kenyan coffee",
+        price: 10,
+        image:  {kenyan},
+    },
+    {
+        id: 6,
+        name: "Morocan coffee",
+        price: 10,
+        image:  {morocan},
+    },
+    {
+        id: 7,
+        name: "New Orleans coffee",
+        price: 10,
+        image:  {neworleans},
+    },
+    {
+        id: 8,
+        name: "Vienesse coffee",
+        price: 10,
+        image:  {viennese},
+    }
+]
